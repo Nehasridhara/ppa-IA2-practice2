600b8aeb572c4e18b3620f0e4357e9c45eaac802
Nehasridhara <95270534+Nehasridhara@users.noreply.github.com>
2022-02-08 06:20:38 +0000

    done

--- p1original.c
+++ p1original.c
@@ -1,2 +1,33 @@
-
+#include <stdio.h>
+void input(float *base,float *height)
+{
+  printf("enter the values:\n");
+  scanf("%f%f",base,height);
 }
+
+void area(float base,float height,float*A)
+{
+  *A=((base*height)/2);
+}
+
+void output(float base,float height,float A)
+{
+  printf("Area of triangle is ((%f*%f)/2))=%f:\n",base,height,A);
+}
+
+int main()
+{
+  float base,height,A;
+  input(&base,&height);
+  area(base,height,&A);
+  output(base,height,A);
+  return 0;
+}
+
+
+
+
+
+
+
+
